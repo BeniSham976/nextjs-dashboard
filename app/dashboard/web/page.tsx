dfbca383f41430e7e4585474a7b510e7bda4e604
Beni Shamavu <d00253976@student.dkit.ie>
2025-03-14 10:58:41 +0000

    Completed version for CA2

--- app/dashboard/web/page.tsx
+++ app/dashboard/web/page.tsx
@@ -1,19 +1,21 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 import Section1Image from 'next/image';
 import Section2Image from 'next/image';
+import styles from '@/app/ui/home.module.css';
 
 export default function Page() {
-    // ..
-    <div className="bg-gradient-to-t from-[#fbc2eb] to-[#a6c1ee]">
-    <main className="flex min-h-screen flex-col p-6">
-    <div className="flex h-20 shrink-0 items-end rounded-lg p-4 md:h-52">
-      </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+    return ( 
+      // ...
+      <div className="bg-gradient-to-t from-[#fbc2eb] to-[#a6c1ee]">
+      <main className="flex min-h-screen flex-col p-6">
+          <div className="bg-orange-500 mx-2">
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
       </p>
-      <h2><strong>Web</strong></h2> Python is a object-oriented, high-level programming language that is very popular and often reccomended for beginners looking to learn their first programming language. Apps such as Spotify, Google Search and Instagram use Python for its speed, efficiency and easy maintenance of their respective apps. The programming language is easy to learn, unlike C++ or Java{' '}
-        </div>
+      <h2><strong>Web</strong></h2> Start creating great webpages using HTML and customising the appearance, colours and fonts using CSS. JavaScript enables the webpage to be interactive, create various objects such as hamburger and dropdown menus and much, much more. Great thing about HTML, CSS and JavaScript is that no install is required so you can jump right in!{' '}
+        </div><br></br>
         <div className="lg:grid lg:grid-cols-2 lg:w-4/5 lg:gap-6 lg:justify-center lg:mx-auto xl:grid xl:grid-cols-2 xl:w-4/5 xl:gap-6 xl:justify-center 2xl:mx-auto 2xl:grid 2xl:grid-cols-2 2xl:w-4/5 2xl:gap-6 2xl:justify-center xl:mx-auto md:grid md:grid-cols-2 md:gap-4 md:mx-auto">
         <section className="bg-blue-500 rounded-xl" id='one'>
         <Section1Image
@@ -22,8 +24,8 @@ export default function Page() {
           height={760}
           alt="Section 2 image">
           </Section1Image>
-        <h2>Introduction to Python</h2>
-        In the introduction of Python, it includes topics such as installation, running your first Python program and understanding the syntax for Python. After gaining the foundation skills to use Python, we can begin to use Python for small animation, app and interactive projects.
+        <h2>Introduction to Web</h2>
+        In this introduction to Web Design, beginners will learn to add structure and style to webpages by adding images, fonts, lists, links quotes and animations.
         </section>
 
         <section className='bg-purple-500 rounded-xl' id='two'>
@@ -33,11 +35,11 @@ export default function Page() {
           height={760}
           alt="Section 2 image">
         </Section2Image>
-        <h2>More Python</h2>
-        This is the next step up from the Introduction to Scratch. In this section, you will create apps, games if...then conditions and variables. Using the skills you built on in the beginning, the projects can become more complex, which will display your higher levels of programming skills.
+        <h2>More Web</h2>
+        Moving on from the introduction, we head into Modern Web Design. In the early days of the World Wide Web, webpages were static and boring walls of text. Thanks to the advancements in Web Design, we can bring fun and engagine experiences to users. Using your creativity, take users on a journey somewhere new.
         </section>
         </div>
+      </main>
       </div>
-    </main>
-    </div>
+    );
   }
